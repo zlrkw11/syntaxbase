df91f019d29329fb03a7edfd648034d4469f79b7
ZL <ray040406@gmail.com>
2025-04-09 23:07:07 +1200

    type inference

---
+++ languages/typescript/basics/type_inference.ts
@@ -0,0 +1,11 @@
+// default to be string
+let randomName = "ray";
+// cannot do this: randomName = 7;
+
+// same as:
+let randomName2: string = "ray";
+
+// if not assigned, it will be any type
+let randomName3;
+randomName3 = "ray";
+randomName3 = 7;
