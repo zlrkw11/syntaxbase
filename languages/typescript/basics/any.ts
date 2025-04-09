b298410d081b77ca5365f44af2a1af6268c7751e
ZL <ray040406@gmail.com>
2025-04-09 23:02:32 +1200

    any type

--- languages/typescript/basics/any.ts
+++ languages/typescript/basics/any.ts
@@ -1,2 +1,15 @@
-// cannot assin
+// cannot assign
 let myName: string = "ray";
+
+// can change
+let myName2: any = "ray";
+
+// can access any properties even they don't exist
+let anyThing: any = "value";
+console.log(anyThing.name);
+console.log(anyThing.name.firstName);
+
+let anyThing2: any = "value";
+anyThing2.setName("ray");
+anyThing2.setName("ray").sayHello();
+anyThing2.setName("ray").setFirstName("Cat");
