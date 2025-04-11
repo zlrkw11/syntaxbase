b30dec0ad56ce368375416de6bb7c12f8995bc93
zlrkw11 <ray040406@gmail.com>
2025-04-11 17:50:05 +1200

    api error type assertion

--- languages/typescript/basics/code/type_assertion.ts
+++ languages/typescript/basics/code/type_assertion.ts
@@ -2,12 +2,30 @@ interface Car {
   name: string;
   drive(): void;
 }
+
 interface Truck {
   name: string;
   store(): void;
 }
+
 function isTruck(vehicle: Car | Truck) {
   if (typeof (vehicle as Truck).store === "function") {
+    return true;
+  }
+  return false;
+}
+
+// inheritance
+class ApiError extends Error {
+  code: number = 0;
+}
+
+class HttpError extends Error {
+  statusCode: number = 200;
+}
+
+function isApiError(error: Error) {
+  if (typeof (error as ApiError).code === "number") {
     return true;
   }
   return false;
