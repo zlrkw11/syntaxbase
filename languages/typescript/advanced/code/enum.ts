f9d221abe8a070fb7f713cf3e7b92666e66bacc7
ZL <ray040406@gmail.com>
2025-04-20 14:09:26 +1200

    feat: enum

---
+++ languages/typescript/advanced/code/enum.ts
@@ -0,0 +1,31 @@
+enum Days {
+  Sun,
+  Mon,
+  Tue,
+  Wed,
+  Thu,
+  Fri,
+  Sat,
+}
+
+enum Cars {
+  Toyota = 1,
+  Honda = 7,
+  Ford, // 8
+  BMW, // 9
+  Mercedes, // 10
+}
+
+const enum Directions {
+  Up,
+  Down,
+  left,
+  Right,
+}
+
+let directions = [
+  Directions.Up,
+  Directions.Down,
+  Directions.left,
+  Directions.Right,
+];
