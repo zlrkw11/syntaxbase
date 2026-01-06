f7df96f5b270534b4a44e09b81d9462c02388d6e
ZL <ray040406@gmail.com>
2026-01-06 19:30:36 +1300

    feat: 0002

--- languages/typescript/basics/code/union_type.ts
+++
@@ -1,9 +0,0 @@
-let myFavouriteNumber: string | number;
-
-// the variable can choose a type from all the types declared
-myFavouriteNumber = "one";
-myFavouriteNumber = 1;
-
-function getString(something: string | number): string {
-  return something.toString();
-}
